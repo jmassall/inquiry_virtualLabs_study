41e6cb88353ec826dd2702e390fecd5bd8b1cd31
sperez8 <karatezeus21@gmail.com>
2017-10-31 14:08:37 -0700

    Reorganized timeline making functions and add some scripts to run parsing on new raw log files

--- parsing_log/make_cleaning_report_only.py
+++ parsing_log/make_cleaning_report_only.py
@@ -10,23 +10,21 @@ import json
 from utils import *
 import getpass
 
-datapath = 'C:\\Users\\'+getpass.getuser()+'\\Documents\\Personal Content\\Lab_skills_study\\raw study data\\log data\\'
-outpath =  'C:\\Users\\'+getpass.getuser()+'\\Documents\\Personal Content\\Lab_skills_study\\cleaned log data'
+datapath = 'C:\\Users\\'+getpass.getuser()+'\\Desktop\\hunt for missing logs\\'
+# outpath =  'C:\\Users\\'+getpass.getuser()+'\\Documents\\Personal Content\\Lab_skills_study\\cleaned log data'
 # rawfile = '43abdd26-76bd-4fe9-9f7b-29500369038f.log' #all logs - beers
-rawfile = '38663fa4-7ac5-4868-b687-82d9aa05ab37.log' #all logs caps
+# rawfile = '38663fa4-7ac5-4868-b687-82d9aa05ab37.log' #all logs caps
+# rawfile = "e92af660-e970-4c28-b3e2-e09ad0fe3963.log" #redownloaded beers logs
+rawfile = "7a95ceca-40b6-4d54-99f4-6bec8f161524.log" #redownloaded beers logs
 
-# datapath = 'C:\\Users\\'+getpass.getuser()+'\\Desktop\\hunt for missing logs\\'
-# outpath =  'C:\\Users\\'+getpass.getuser()+'\\Desktop\\hunt for missing logs\\'
-# # rawfile = 'e92af660-e970-4c28-b3e2-e09ad0fe3963.log' #all logs - beers
-# rawfile = '7a95ceca-40b6-4d54-99f4-6bec8f161524.log' #all logs caps
 
-split_data_path = os.path.join(outpath,'cleaned_and_split_' + rawfile.split('.')[0])
+# split_data_path = os.path.join(outpath,'cleaned_and_split_' + rawfile.split('.')[0])
 
-#create a folder for the new data files, if one doesn't already exist.
-if not os.path.exists(split_data_path):
-    os.makedirs(split_data_path)
+# #create a folder for the new data files, if one doesn't already exist.
+# if not os.path.exists(split_data_path):
+#     os.makedirs(split_data_path)
 
-report_path = os.path.join(outpath,'report_of_cleaning_of_file_' + rawfile.split('.')[0] +'.txt')
+report_path = os.path.join('C:\\Users\\'+getpass.getuser()+'\\Desktop','report_of_cleaning_of_file_' + rawfile.split('.')[0] +'.txt')
 report = open(report_path, 'w')
 report.write('\t'.join(['session id','student id','id has 8 digits', 'id start with 1','sim','date','number of events before cleaning','number of events after cleaning','file name']))
 
@@ -46,14 +44,14 @@ with open(os.path.join(datapath+rawfile),'r') as f:
         before_cleaning = len(session.events)
         session.clean_events()  #removes all time tracking and mouse tracking events
         outname = "log_{0}_{1}_{2}.json".format(session.sim, session.student_id, session.date)
-        with open(os.path.join(split_data_path,outname), 'w') as outfile:
-            #if you want pretty print, use print >> as below instead of just json.dump
-            # print >> outfile, json.dumps(session.events, indent=4, sort_keys=True)
-            json.dump(session.events, outfile)
+        # with open(os.path.join(split_data_path,outname), 'w') as outfile:
+        #     #if you want pretty print, use print >> as below instead of just json.dump
+        #     # print >> outfile, json.dumps(session.events, indent=4, sort_keys=True)
+        #     json.dump(session.events, outfile)
         #we report the metadata on all the session in a separate file.
         report.write('\n')
         report.write('\t'.join([session.session_id,session.student_id,str(check_id_length(session.student_id)), str(check_id_start(session.student_id)),session.sim, session.date,str(before_cleaning), str(len(session.events)), outname]))
-        outfile.close()
+        # outfile.close()
 
 f.close()
 report.close()
